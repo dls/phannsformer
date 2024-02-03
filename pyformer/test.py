3951dccbc144cca2abd8157f1269efb21be81b94
Sean <sfahey1745@sdsu.edu>
2024-02-03 11:17:44 -0800

    'print better'

--- pyformer/test.py
+++ pyformer/test.py
@@ -112,7 +112,9 @@ def test(dataloader, model, loss_fn):
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-            print(f'\tResult|true:{y}|pred:{pred}')
+            pred_idx = pred.argmax(1)
+            for i in range(len(y)):
+                print(f'\tResult|true:{y[i]}|pred:{pred_idx[i]}')
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
